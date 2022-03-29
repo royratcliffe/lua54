b5a0b487ec7089d6e6b9ded13143efeffd45bf41
Roy Ratcliffe <royratcliffe@me.com>
2022-03-29 16:35:34 +0100

    feat: stream trampolines

--- c/lua_prolog.c
+++ c/lua_prolog.c
@@ -10,6 +10,73 @@ l_alloc(void *mem0, void *mem, size_t size0, size_t size)
   return PL_realloc(mem, size);
 }
 
+struct io_stream *l_open_pipe(const char *cmd, const char *how)
+{ return Sopen_pipe(cmd, how);
+}
+
+struct io_stream *l_open_file(const char *path, const char *how)
+{ return Sopen_file(path, how);
+}
+
+int l_close(struct io_stream *s)
+{ return Sclose(s);
+}
+
+int l_lock(struct io_stream *s)
+{ return Slock(s);
+}
+
+int l_unlock(struct io_stream *s)
+{ return Sunlock(s);
+}
+
+int l_seek64(struct io_stream *s, off64_t off, int whence)
+{ return Sseek64(s, off, whence);
+}
+
+off64_t l_tell64(struct io_stream *s)
+{ return Stell64(s);
+}
+
+int l_fgetc(struct io_stream *s)
+{ return Sfgetc(s);
+}
+
+int l_ungetc(int c, struct io_stream *s)
+{ return Sungetc(c, s);
+}
+
+size_t l_fread(void *data, size_t size, size_t len, struct io_stream *s)
+{ return Sfread(data, size, len, s);
+}
+
+/*
+ * Writes to a given stream accounting for the stream's encoding. How does a Prolog stream handle UTF-8? Lua uses only UTF-8.
+ *
+ * Returns the number of successfully written elements.
+ */
+size_t l_fwrite(const void *data, size_t size, size_t len, struct io_stream *s)
+{ return Sfwrite(data, size, len, s);
+}
+
+int l_fprintf(struct io_stream *s, const char *f, ...)
+{ va_list a;
+  va_start(a, f);
+  return Svfprintf(s, f, a);
+}
+
+int l_flush(struct io_stream *s)
+{ return Sflush(s);
+}
+
+int l_ferror(struct io_stream *s)
+{ return Sferror(s);
+}
+
+void l_clearerr(struct io_stream *s)
+{ Sclearerr(s);
+}
+
 /*
  * Lua error to Prolog error generically. Converts a Lua status code to
  * a Prolog error and `FALSE` return value, assuming that the Prolog
--- c/lua_prolog.h
+++ c/lua_prolog.h
@@ -1,4 +1,19 @@
 #include "prolog.h"
 
 void *l_alloc(void *mem0, void *mem, size_t size0, size_t size);
+struct io_stream *l_open_pipe(const char *cmd, const char *how);
+struct io_stream *l_open_file(const char *path, const char *how);
+int l_close(struct io_stream *s);
+int l_lock(struct io_stream *s);
+int l_unlock(struct io_stream *s);
+int l_seek64(struct io_stream *s, off64_t off, int whence);
+off64_t l_tell64(struct io_stream *s);
+int l_fgetc(struct io_stream *s);
+int l_ungetc(int c, struct io_stream *s);
+size_t l_fread(void *data, size_t size, size_t len, struct io_stream *s);
+size_t l_fwrite(const void *data, size_t size, size_t len, struct io_stream *s);
+int l_fprintf(struct io_stream *s, const char *f, ...);
+int l_flush(struct io_stream *s);
+int l_ferror(struct io_stream *s);
+void l_clearerr(struct io_stream *s);
 int l_err(int status);
